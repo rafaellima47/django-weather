1974dc305871245307409e13c24f4614ca7a525e
Rafael Lima <47rafael.lima@gmail.com>
2021-07-19 19:38:06 -0300

    Added templates folder and first html files

--- the_weather/settings.py
+++ the_weather/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'the_weather.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
