fd0435c5256bd2737a6ebcd22880f11e46513fcb
Rafael Lima <47rafael.lima@gmail.com>
2021-07-19 21:36:15 -0300

    Added config.py and temporary api request in views.py

--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
+import requests as req
+
+from .config import API_KEY
+
 def index(requests):
-	return render(requests, "weather/index.html")
+	response = req.get("https://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format("london", API_KEY))
+	weather_data = response.json()
+
+	context = {"weather_data": weather_data}
+	return render(requests, "weather/index.html", context)
