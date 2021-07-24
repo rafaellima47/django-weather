159cc2925d9ee90e8e5e22547951480bf68a229c
Rafael Lima <47rafael.lima@gmail.com>
2021-07-23 23:49:31 -0300

    Change to Class Based Views

--- weather/views.py
+++ weather/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.conf import settings
+from django.views.generic import View
 
 import requests as req
 
 
-def index(requests):
-	url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format("london", settings.API_KEY)
-	response = req.get(url)
-	weather_data = response.json()
+class IndexView(View):
 
-	context = {"weather_data": weather_data}
-	return render(requests, "weather/index.html", context)
+	def get(self, request, *args, **kwargs):
+		url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format("london", settings.API_KEY)
+		response = req.get(url)
+		weather_data = response.json()
+
+		context = {"weather_data": weather_data}
+		return render(requests, "weather/index.html", context)
