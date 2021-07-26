74634db305f684c600c93e865eb3dcc81c7015f2
Rafael Lima <47rafael.lima@gmail.com>
2021-07-26 20:14:26 -0300

    Created signup form and finished users templates

--- users/views.py
+++ users/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.views import LoginView
-from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+
+from .forms import SignUpForm
 
 class UsersLoginView(LoginView):
 	template_name = "users/login.html"
 
 
 
-class UsersSignupView(TemplateView):
+class UsersSignupView(CreateView):
 	template_name = "users/signup.html"
+	form_class = SignUpForm
